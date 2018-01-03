1b256e520d57e1f0b2fe1ff6cb6290dab26b2be2
Lucas Vieira <lucasvieira@protonmail.com>
2018-01-03 15:23:08 -0200

    Added smartphone partial support. You can now play with the phone!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #![recursion_limit="2048"]
 
+
+// For later use:
+// In portrait orientation, use device's GAMMA
+// In landscape orientation, use device's BETA
+
 #[macro_use]
 extern crate stdweb;
 #[macro_use]
@@ -30,6 +35,7 @@ use stdweb::web::event::{
 use std::sync::Mutex;
 use stdweb::unstable::TryInto;
 use std::collections::HashMap;
+use std::f64::consts;
 
 // ==============================
 
@@ -44,6 +50,51 @@ enum KeyType {
 //    LMB,
 //    Q,
 }
+
+#[derive(PartialEq)]
+enum OrientationType {
+    Unknown,
+    Portrait,
+    Landscape,
+}
+
+#[derive(Clone)]
+struct DeviceTilt {
+    abs:   f64,
+    alpha: f64,
+    beta:  f64,
+    gamma: f64,
+}
+
+struct TiltState {
+    active: bool,
+    orient: OrientationType,
+    async:  DeviceTilt,
+    old:    DeviceTilt,
+    new:    DeviceTilt,
+}
+
+impl TiltState {
+    fn new() -> TiltState {
+        let tilt = DeviceTilt {
+            abs:   0.0,
+            alpha: 0.0,
+            beta:  0.0,
+            gamma: 0.0,
+        };
+        
+        TiltState {
+            active: false,
+            orient: OrientationType::Unknown,
+            async:  tilt.clone(),
+            old:    tilt.clone(),
+            new:    tilt.clone(),
+        }
+    }
+}
+
+
+
 
 struct KeyState {
     async: HashMap<KeyType, bool>,
@@ -91,6 +142,7 @@ struct World {
     fullscreen:   bool,
 
     input:        KeyState,
+    tilt:         TiltState,
     ball_state:   BallState,
     paddle_state: PaddleState,
 }
@@ -113,6 +165,7 @@ impl World {
             // I already tried using that.
             fullscreen: false,
             input: KeyState::new(),
+            tilt:  TiltState::new(),
             ball_state: BallState {
                 diameter:    0.0,
                 pos:         (0.0, 0.0),
@@ -247,6 +300,18 @@ impl World {
     fn update(&mut self, dt: f64) {
         // Collect input state
         self.input.new = self.input.async.clone();
+        if self.tilt.active {
+            self.tilt.new = self.tilt.async.clone();
+            // Collect device orientation
+            let is_portrait: bool = js!( return (window.innerHeight > window.innerWidth); )
+                .try_into()
+                .unwrap();
+            if is_portrait {
+                self.tilt.orient = OrientationType::Portrait;
+            } else {
+                self.tilt.orient = OrientationType::Landscape;
+            }
+        }
         
         // Process new input
         for (key, state) in &self.input.new {
@@ -303,6 +368,23 @@ impl World {
             }
         }
 
+        // Process mobile input
+        {
+            if self.tilt.orient != OrientationType::Unknown {
+                let ratio = self.tilt.new.gamma as f32 *
+                // Make the tilt a little more violent by narrowing
+                // the distance down, depending on orientation
+                    if self.tilt.orient == OrientationType::Portrait {
+                        3.0
+                    } else { 3.5 };
+
+                let halfwidth = self.vwpsize.0 as f32 / 2.0;
+                self.paddle_state.xpos =  halfwidth +  (halfwidth * ratio);
+            }
+        }
+
+        // ======
+        
         // The following events will only happen if the game is not paused.
         if !self.pause {
 
@@ -392,6 +474,9 @@ impl World {
 
         // Give input to old
         self.input.old = self.input.new.clone();
+        if self.tilt.active {
+            self.tilt.old = self.tilt.new.clone();
+        }
     }
 
 
@@ -403,7 +488,7 @@ impl World {
         // Afterimages
         let mut i: u8 = 0;
         for &afterimage in &self.ball_state.afterimages {
-            let color = format!("#FFFFFF{:02X}", i);
+            let color = format!("#{:02X}{:02X}{:02X}", i, i, i);
             let color = color.as_ref();
             self.draw_circle(color, afterimage, ball_radius);
             i += 13;
@@ -474,6 +559,23 @@ fn on_key(key: &str, _location: KeyboardLocation, pressed: bool) -> bool {
     true
 }
 
+fn on_device_tilt(absolute: f64, alpha: f64, beta: f64, gamma: f64) {
+    let mut world = WORLD.lock().unwrap();
+    world.tilt.active = true;
+
+    // Convert to radians
+    world.tilt.async.abs = absolute.to_radians();
+    world.tilt.async.alpha = alpha.to_radians();
+    world.tilt.async.beta = beta.to_radians();
+    world.tilt.async.gamma = gamma.to_radians();
+}
+
+fn on_touch(pressed: bool) {
+    // WIP
+    WORLD.lock().unwrap().input_dispatch(KeyType::S, pressed);
+}
+
+
 fn game_loop(last_call: f64) {
     let now: f64 = js!( return Date.now(); ).try_into().unwrap();
     let dt:  f64 = now - last_call;
@@ -501,18 +603,84 @@ fn main() {
     //WORLD.lock().unwrap().draw_box("green", (60.0, 60.0), (150.0, 100.0));
 
     // Bind event listeners
+    // Key down event
     web::window().add_event_listener(|event: KeydownEvent| {
         if on_key(&event.key(), event.location(), true) {
             event.prevent_default();
         }
     });
-
+    
+    // Key up event
     web::window().add_event_listener(|event: KeyupEvent| {
         if on_key(&event.key(), event.location(), false) {
             event.prevent_default();
         }
     });
 
+    // Device orientation event.
+    // Needs to be done in pure JS, since we still don't have Rust
+    // bindings...
+    js! {
+        // Expose handler functions
+        Module.exports.deviceTiltCallback  = @{on_device_tilt};
+        Module.exports.deviceTouchCallback = @{on_touch};
+
+        // Outsource events to WASM framework by using an event listener
+        if (@{web::window()}.DeviceOrientationEvent) {
+            @{web::window()}.addEventListener("deviceorientation", function (e) {
+                e.preventDefault();
+
+                // Sorry, I know this is horrible, but this is the only
+                // way I found to "cast" these values to floats in JS.
+                // Damn untyped languages.
+                var abs = 0.0;
+                var alpha = 0.0;
+                var beta = 0.0;
+                var gamma = 0.0;
+                
+                abs += e.absolute;
+                alpha += e.alpha;
+                beta += e.beta;
+                gamma += e.gamma;
+                
+                Module.exports.deviceTiltCallback(abs, alpha, beta, gamma);
+            }, false);
+        } else if (@{web::window()}.DeviceMotionEvent) {
+            @{web::window()}.addEventListener("devicemotion", function (e) {
+                e.preventDefault();
+
+                // Same as above, but I didn't test these.
+                // Might not be needed.
+                var alpha = 0.0;
+                var beta = 0.0;
+                var gamma = 0.0;
+                
+                alpha += e.acceleration.z * 2.0;
+                beta += e.acceleration.x * 2.0;
+                gamma += e.acceleration.y * 2.0;
+                
+                Module.exports.deviceTiltCallback(0.0,
+                                                  alpha,
+                                                  beta,
+                                                  gamma);
+            }, false);
+        } else {
+            alert("Sorry, your phone sucks");
+        }
+
+        if (@{web::window()}.TouchEvent) {
+            @{web::window()}.addEventListener("touchstart", (e) => {
+                Module.exports.deviceTouchCallback(true);
+            }, false);
+
+            @{web::window()}.addEventListener("touchend", (e) => {
+                Module.exports.deviceTouchCallback(false);
+            }, false);
+        }
+    };
+
+    // This starts game loop by calling it on the
+    // next available animation frame
     web::window().request_animation_frame( |_| {
         game_loop(0.0);
     });
